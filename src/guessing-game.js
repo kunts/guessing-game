16af84aab31134a07bf5dc12d35fd63f49bd54e6
Alena Kuntsevich <ekuntsevich@yandex.ru>
2017-02-02 11:06:49 +0300

    guessing-game/feature: is done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+
+
+    }
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
+
+
 
     }
 
     guess() {
-
+        var mid = Math.round((this.max+this.min)/2);
+  return mid;
     }
 
     lower() {
-
+    this.max= Math.round((this.max+this.min)/2);
     }
 
     greater() {
-
+        this.min= Math.round((this.max+this.min)/2);
     }
 }
 
